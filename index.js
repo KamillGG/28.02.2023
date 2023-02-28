0e490febb0f949d72891da10979bd0cf69325db7
KamilGmitrzuk <gmitrzukkamil@gmail.com>
2023-02-28 15:24:34 +0100

    imie i nazwisko

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ app.use(cors())
 
 const port = 3000
 
-app.get("/", function(req,res){
-    res.send("ok")
+app.get("/:imie/:nazwisko", function(req,res){
+    const imie = req.params.imie
+    const nazwisko = req.params.nazwisko
+    res.send(`${imie} ${nazwisko}`)
+    console.log(`${imie} ${nazwisko}`)
 })
 
 
